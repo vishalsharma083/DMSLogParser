24382ba7343ec04961cb9a99f2db90f23f0e2181
vishal <vishal@fareportal.com>
2013-05-03 21:36:40 +0530

    added sql script

---
+++ createtable.sql
@@ -0,0 +1,8 @@
+﻿
+CREATE TABLE [dbo].[DMSLog](
+	[LogDateTime] [datetime] NULL,
+	[DealId] [varchar](100) NULL,
+	[Params] [varchar](4000) NULL,
+	[LineNumber] [int] NULL,
+	[DMSRemark] [varchar](4000) NULL
+) ON [PRIMARY]
